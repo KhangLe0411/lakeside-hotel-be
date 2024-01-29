8e834199c727ca61c2ca93dda3f55a88e80e668a
KhangLe0411 <khangle04112002@gmail.com>
2024-01-29 17:32:25 +0700

    update booked controller

--- src/main/java/com/hkcode/lakesidehotel/controller/BookedRoomController.java
+++ src/main/java/com/hkcode/lakesidehotel/controller/BookedRoomController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/bookings")
+@CrossOrigin("http://localhost:5173")
 public class BookedRoomController {
     private final IBookedRoomService bookingService;
     private final IRoomService roomService;
